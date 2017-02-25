ddbde64bb7a2d2dc2717aad3756d0ea825cf9dd6
coreGreenberet <green@corona-bytes.net>
2017-02-25 22:22:43 +0100

    _restCall -> made body parameter optional

--- homematicip/HomeMaticIPObject.py
+++ homematicip/HomeMaticIPObject.py
@@ -13,7 +13,7 @@ class HomeMaticIPObject(object):
         self.headers = {'content-type': 'application/json', 'accept': 'application/json', 'VERSION': '8',
                         'AUTHTOKEN': homematicip.get_auth_token()}
 
-    def _restCall(self, path, body):
+    def _restCall(self, path, body = None):
         result = None
         result = requests.post('{}/hmip/{}'.format(homematicip.get_urlREST(), path), data=body, headers=self.headers)
         return result.json()
