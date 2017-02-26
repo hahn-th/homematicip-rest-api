ab4ed0ce44c91dcc8ea094f3ad6dbe8d182f1a2c
CodeNightWriter <Code@NightWriter.com>
2017-02-26 16:28:10 +0100

    tested support for HMIP-WTH added support for HMIP-STHD and HMIP-WRC2

--- homematicip/device.py
+++ homematicip/device.py
@@ -115,6 +115,30 @@ class WallMountedThermostatPro(Device):
         return u"{}: actualTemperature({}) humidity({})".format(super(WallMountedThermostatPro, self).__unicode__(),
                                                                 self.actualTemperature, self.humidity)
 
+class TemperatureHumiditySensorDisplay(Device):
+    temperatureOffset = None
+    display = None
+    actualTemperature = None
+    humidity = None
+
+    def from_json(self, js):
+        super(TemperatureHumiditySensorDisplay, self).from_json(js)
+        for cid in js["functionalChannels"]:
+            c = js["functionalChannels"][cid]
+            type = c["functionalChannelType"]
+            if type == "WALL_MOUNTED_THERMOSTAT_PRO_CHANNEL":
+                self.temperatureOffset = c["temperatureOffset"]
+                self.display = c["display"]
+                self.actualTemperature = c["actualTemperature"]
+                self.humidity = c["humidity"]
+
+            elif type == "DEVICE_BASE":
+                self.unreach = c["unreach"]
+                self.lowBat = c["lowBat"]
+
+    def __unicode__(self):
+        return u"{}: actualTemperature({}) humidity({})".format(super(TemperatureHumiditySensorDisplay, self).__unicode__(),
+                                                                self.actualTemperature, self.humidity)
 
 class SmokeDetector(Device):
     smokeDetectorAlarmType = None
@@ -183,3 +207,18 @@ class PlugableSwitchMeasuring(Device):
 
     def turn_off(self):
         return self.set_switch_state(False)
+
+
+class PushButton(Device):
+
+    def from_json(self, js):
+        super(PushButton, self).from_json(js)
+        for cid in js["functionalChannels"]:
+            c = js["functionalChannels"][cid]
+            type = c["functionalChannelType"]
+            if type == "DEVICE_BASE":              
+                self.unreach = c["unreach"]
+                self.lowBat = c["lowBat"]
+
+    def __unicode__(self):
+        return u"{}".format(super(PushButton, self).__unicode__())
