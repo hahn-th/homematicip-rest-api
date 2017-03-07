97a4fe9116c6a5962c8847b821f85eec0c186cbe
coreGreenberet <green@corona-bytes.net>
2017-03-07 19:28:59 +0100

    moved from optparse to argparse

--- homematicip_cli.py
+++ homematicip_cli.py
@@ -1,7 +1,7 @@
 import config
 import logging
 from operator import attrgetter
-from optparse import OptionParser, OptionGroup
+from argparse import ArgumentParser
 import sys
 import homematicip
 
@@ -16,36 +16,33 @@ def create_logger():
 logger = create_logger();
 
 
-
-
 def main():
-    usage = "usage: %prog [options]"
-    parser = OptionParser(usage)
-    parser.add_option("", "--debug-level", dest="debug_level", type="int", default=30, help="the debug level which should get used(Critical=50, DEBUG=10)")
+    parser = ArgumentParser(description="a cli wrapper for the homematicip API")
+    parser.add_argument("--debug-level", dest="debug_level", type=int, default=30, help="the debug level which should get used(Critical=50, DEBUG=10)")
 
-    group = OptionGroup(parser,"Display Configuration")
-    group.add_option("", "--list-devices", action="store_true", dest="list_devices", help="list all devices")
-    group.add_option("", "--list-groups", action="store_true", dest="list_groups", help="list all groups")
-    group.add_option("", "--list-firmware", action="store_true", dest="list_firmware", help="list the firmware of all devices")
-    parser.add_option_group(group)
+    group = parser.add_argument_group("Display Configuration")
+    group.add_argument("--list-devices", action="store_true", dest="list_devices", help="list all devices")
+    group.add_argument("--list-groups", action="store_true", dest="list_groups", help="list all groups")
+    group.add_argument("--list-firmware", action="store_true", dest="list_firmware", help="list the firmware of all devices")
+    
 
-    parser.add_option("", "--list-security-journal", action="store_true", dest="list_security_journal", help="display the security journal")
-    group.add_option("-d", "--device", dest="device", help="the device you want to modify (see \"Device Settings\")")
+    parser.add_argument("--list-security-journal", action="store_true", dest="list_security_journal", help="display the security journal")
+    parser.add_argument("-d", "--device", dest="device", help="the device you want to modify (see \"Device Settings\")")
 
-    group = OptionGroup(parser,"Device Settings")
-    group.add_option("", "--turn_on", action="store_true", dest="device_switch_state", help="turn the switch on")
-    group.add_option("", "--turn_off", action="store_false", dest="device_switch_state", help="turn the switch off")
-    group.add_option("", "--set-label", dest="device_new_label", help="set a new label")
-    group.add_option("", "--set-display", dest="device_display", type="choice", action="store", help="set the display mode", choices=["actual","setpoint", "actual_humidity"])
-    parser.add_option_group(group)   
+    group = parser.add_argument_group("Device Settings")
+    group.add_argument("--turn_on", action="store_true", dest="device_switch_state", help="turn the switch on")
+    group.add_argument("--turn_off", action="store_false", dest="device_switch_state", help="turn the switch off")
+    group.add_argument("--set-label", dest="device_new_label", help="set a new label")
+    group.add_argument("--set-display", dest="device_display", action="store", help="set the display mode", choices=["actual","setpoint", "actual_humidity"])
+      
 
     if len(sys.argv) == 1:
         parser.print_help()
         return
 
-    (options, args) = parser.parse_args()
+    args = parser.parse_args()
 
-    logger.setLevel(options.debug_level)
+    logger.setLevel(args.debug_level)
 
     homematicip.init(config.ACCESS_POINT)
     homematicip.set_auth_token(config.AUTH_TOKEN)
@@ -54,46 +51,46 @@ def main():
     home.get_current_state()
 
 
-    if options.list_devices:
+    if args.list_devices:
         sortedDevices = sorted(home.devices, key=attrgetter('deviceType', 'label'))
         for d in sortedDevices:
             print d.id, unicode(d)
 
-    elif options.list_groups:
+    elif args.list_groups:
         sortedGroups = sorted(home.groups, key=attrgetter('groupType', 'label'))
         for g in sortedGroups:
             print unicode(g)
        
-    elif options.list_firmware:
+    elif args.list_firmware:
         sortedDevices = sorted(home.devices, key=attrgetter('deviceType', 'label'))
         for d in sortedDevices:
             print unicode(u"{:45s} - Firmware: {:6s} - Available Firmware: {} UpdateState: {}".format(d.label, d.firmwareVersion,
                                                                                       d.availableFirmwareVersion, d.updateState))
-    elif options.list_security_journal:
+    elif args.list_security_journal:
         journal = home.get_security_journal()
         for entry in journal:
             print unicode(entry)
-    elif options.device:
+    elif args.device:
         device = None
         for d in home.devices:
-            if d.id == options.device:
+            if d.id == args.device:
                 device = d
                 break
         if device == None:
-            logger.error("Could not find device {}".format(options.device))
+            logger.error("Could not find device {}".format(args.device))
             return
 
-        if options.device_new_label:
-            device.set_label(options.device_new_label)
-        if options.device_switch_state != None:
+        if args.device_new_label:
+            device.set_label(args.device_new_label)
+        if args.device_switch_state != None:
             if isinstance(device, homematicip.PlugableSwitch):
-                device.set_switch_state(options.device_switch_state)
+                device.set_switch_state(args.device_switch_state)
             else:
                 logger.error("can't turn on/off device {} of type {}".format(device.id,device.deviceType))
 
-        if options.device_display != None:
+        if args.device_display != None:
             if isinstance(device, homematicip.TemperatureHumiditySensorDisplay):
-                device.set_display(options.device_display.upper())
+                device.set_display(args.device_display.upper())
             else:
                 logger.error("can't set display of device {} of type {}".format(device.id,device.deviceType))
 
