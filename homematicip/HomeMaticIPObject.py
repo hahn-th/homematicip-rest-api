4eb66b81feb4f12cee47c7fb63b418fad3d81e2d
coreGreenberet <green@corona-bytes.net>
2017-03-10 17:42:14 +0100

    fixed unlimited timeout on _restCall

--- homematicip/HomeMaticIPObject.py
+++ homematicip/HomeMaticIPObject.py
@@ -7,17 +7,25 @@ import logging
 logger = logging.getLogger(__name__)
 
 class HomeMaticIPObject(object):
-    """description of class"""
+    """This class represents a generic homematic ip object to make basic requests to the access point"""
     headers = {}
-
+    _restCallRequestCounter = 3 #the homematic ip cloud tends to time out. retry the call X times.
+    _restCallTimout = 3
     def __init__(self):
         self.headers = {'content-type': 'application/json', 'accept': 'application/json', 'VERSION': '8',
                         'AUTHTOKEN': homematicip.get_auth_token()}
 
     def _restCall(self, path, body = None):
         result = None
-        result = requests.post('{}/hmip/{}'.format(homematicip.get_urlREST(), path), data=body, headers=self.headers)
-        return result.json() if result.content != "" else ""
+        requestPath = '{}/hmip/{}'.format(homematicip.get_urlREST(), path)
+        for i in xrange(0,self._restCallRequestCounter):
+            try:
+                result = requests.post(requestPath, data=body, headers=self.headers, timeout=self._restCallTimout)
+                return (result.json() if result.content != "" else "")
+            except requests.Timeout:
+                logger.error("call to '{}' failed due Timeout".format(requestPath))
+                pass
+        return { "errorCode" : "TIMEOUT" }
 
 
     def from_json(self, js):
