fe18c4da45d87c06ff7acea7430d64df67d47ff9
coreGreenberet <green@corona-bytes.net>
2017-03-10 17:51:03 +0100

    setup.py update for new tag

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'homematicip',
   packages = ['homematicip'], 
-  version = '0.4',
+  version = '0.5',
   description = 'An API for the homematicip cloud',
   author = 'Heimo Stieg',
   author_email = 'stieg@corona-bytes.net',
   url = 'https://github.com/coreGreenberet/homematicip-rest-api', 
-  download_url = 'https://github.com/coreGreenberet/homematicip-rest-api/tarball/0.4', 
+  download_url = 'https://github.com/coreGreenberet/homematicip-rest-api/tarball/0.5', 
   keywords = ['homematicip'], # arbitrary keywords
   classifiers = [],
 )
