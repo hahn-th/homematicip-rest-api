98afb77217dee371123ab5ea1d27df73293833da
coreGreenberet <green@corona-bytes.net>
2017-06-19 19:01:35 +0200

    updated pip setup for tag 0.7

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'homematicip',
   packages = ['homematicip'], 
-  version = '0.6',
+  version = '0.7',
   description = 'An API for the homematicip cloud',
   author = 'Heimo Stieg',
   author_email = 'stieg@corona-bytes.net',
   url = 'https://github.com/coreGreenberet/homematicip-rest-api', 
-  download_url = 'https://github.com/coreGreenberet/homematicip-rest-api/tarball/0.6', 
+  download_url = 'https://github.com/coreGreenberet/homematicip-rest-api/tarball/0.7', 
   keywords = ['homematicip'], # arbitrary keywords
   classifiers = [],
-  install_requires = ["requests", "websocket-client"]
+  install_requires = ["requests", "websocket-client", "future"]
 )
