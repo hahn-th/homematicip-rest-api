c6c426011cc62c739cdaad2465edbcbe5e940fc9
gar00u <32190251+gar00u@users.noreply.github.com>
2017-11-08 14:49:25 +0100

    manual merge

--- homematicip/home.py
+++ homematicip/home.py
@@ -16,50 +16,6 @@ import sys
 logger = logging.getLogger(__name__)
 
 
-_typeClassMap = {"HEATING_THERMOSTAT": HeatingThermostat,
-                 "SHUTTER_CONTACT": ShutterContact,
-                 "SHUTTER_CONTACT_INVISIBLE" : ShutterContact,
-                 "WALL_MOUNTED_THERMOSTAT_PRO": WallMountedThermostatPro,
-                 "BRAND_WALL_MOUNTED_THERMOSTAT": WallMountedThermostatPro,
-                 "SMOKE_DETECTOR": SmokeDetector,
-                 "FLOOR_TERMINAL_BLOCK_6": FloorTerminalBlock6,
-                 "PLUGABLE_SWITCH_MEASURING": PlugableSwitchMeasuring,
-                 "TEMPERATURE_HUMIDITY_SENSOR_DISPLAY": TemperatureHumiditySensorDisplay,
-                 "PUSH_BUTTON": PushButton,
-                 "ALARM_SIREN_INDOOR": AlarmSirenIndoor,
-                 "MOTION_DETECTOR_INDOOR": MotionDetectorIndoor,
-                 "KEY_REMOTE_CONTROL_ALARM": KeyRemoteControlAlarm,
-                 "PLUGABLE_SWITCH": PlugableSwitch,
-                 "FULL_FLUSH_SHUTTER": FullFlushShutter,
-                 "BRAND_SHUTTER": FullFlushShutter}
-
-_typeGroupMap = {"SECURITY": SecurityGroup, "SWITCHING": SwitchingGroup,
-                 "EXTENDED_LINKED_SWITCHING": ExtendedLinkedSwitchingGroup
-    , "LINKED_SWITCHING": LinkedSwitchingGroup,
-                 "ALARM_SWITCHING": AlarmSwitchingGroup,
-                 "HEATING_HUMIDITY_LIMITER": HeatingHumidyLimiterGroup
-    , "HEATING_TEMPERATURE_LIMITER": HeatingTemperatureLimiterGroup,
-                 "HEATING_CHANGEOVER": HeatingChangeoverGroup,
-                 "INBOX": InboxGroup
-    , "SECURITY_ZONE": SecurityZoneGroup, "HEATING": HeatingGroup,
-                 "HEATING_COOLING_DEMAND": HeatingCoolingDemandGroup
-    , "HEATING_EXTERNAL_CLOCK": HeatingExternalClockGroup,
-                 "HEATING_DEHUMIDIFIER": HeatingDehumidifierGroup
-    , "HEATING_COOLING_DEMAND_BOILER": HeatingCoolingDemandBoilerGroup,
-                 "HEATING_COOLING_DEMAND_PUMP": HeatingCoolingDemandPumpGroup
-    , "SWITCHING_PROFILE": SwitchingProfileGroup,
-                 "OVER_HEAT_PROTECTION_RULE": OverHeatProtectionRule,
-                 "SMOKE_ALARM_DETECTION_RULE": SmokeAlarmDetectionRule,
-                 "LOCK_OUT_PROTECTION_RULE": LockOutProtectionRule,
-                 "SHUTTER_WIND_PROTECTION_RULE": ShutterWindProtectionRule}
-
-_typeSecurityEventMap = {"SILENCE_CHANGED": SilenceChangedEvent,
-                         "ACTIVATION_CHANGED": ActivationChangedEvent,
-                         "ACCESS_POINT_CONNECTED": AccessPointConnectedEvent,
-                         "ACCESS_POINT_DISCONNECTED": AccessPointDisconnectedEvent,
-                         "SENSOR_EVENT": SensorEvent}
-
-
 class Weather(HomeMaticIPObject.HomeMaticIPObject):
     temperature = 0.0
     weatherCondition = "CLEAR"
