7e95cf472d9900bc1a84d140e034b0e1678b318f
sanders76 <s.teunissen@gmail.com>
2017-11-19 20:16:57 +0100

    Updating setup.py script

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 from distutils.core import setup
+from setuptools import find_packages
+
+VERSION = '0.7'
+
 setup(
-  name = 'homematicip',
-  packages = ['homematicip'], 
-  version = '0.7',
-  description = 'An API for the homematicip cloud',
-  author = 'Heimo Stieg',
-  author_email = 'stieg@corona-bytes.net',
-  url = 'https://github.com/coreGreenberet/homematicip-rest-api', 
-  download_url = 'https://github.com/coreGreenberet/homematicip-rest-api/tarball/0.7', 
-  keywords = ['homematicip'], # arbitrary keywords
-  classifiers = [],
-  install_requires = ["requests", "websocket-client", "future"]
+    name='homematicip',
+    packages=find_packages(exclude='tests'),
+    version=VERSION,
+    description='An API for the homematicip cloud',
+    author='Heimo Stieg',
+    author_email='stieg@corona-bytes.net',
+    url='https://github.com/coreGreenberet/homematicip-rest-api',
+    download_url='https://github.com/coreGreenberet/homematicip-rest-api/tarball/' + VERSION,
+    keywords=['homematicip'],  # arbitrary keywords
+    classifiers=[],
+    install_requires=["requests", "websocket-client", "future", "aiohttp"]
 )
