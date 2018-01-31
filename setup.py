810c03a850752e42ddb2aab2eb2ef126386fed54
coreGreenberet <green@corona-bytes.net>
2018-01-31 10:48:52 +0100

    updated classifiers for pip setup

--- setup.py
+++ setup.py
@@ -13,6 +13,6 @@ setup(
     url='https://github.com/coreGreenberet/homematicip-rest-api',
     download_url='https://github.com/coreGreenberet/homematicip-rest-api/tarball/' + VERSION,
     keywords=['homematicip'],  # arbitrary keywords
-    classifiers=[],
+    classifiers=['Development Status :: 4 - Beta', 'Programming Language :: Python :: 3 :: Only'],
     install_requires=["requests", "websocket-client", "future", "aiohttp"]
 )
