d3f9663c2b0d2cd732633bd65332b6da876cbd95
coreGreenberet <green@corona-bytes.net>
2018-02-08 16:00:41 +0100

    fix #28

--- generate_auth_token.py
+++ generate_auth_token.py
@@ -1,41 +1,60 @@
-import homematicip
+mport homematicip
 import homematicip.auth
 import time
 from builtins import input
-
+import json
 
 from homematicip.home import Home
 
-while True:
-    access_point = input("Please enter the accesspoint id (SGTIN): ").replace('-', '').upper()
-    if len(access_point) != 24:
-        print( "Invalid access_point id")
-        continue
-    break
-pin = input("Please enter the PIN (leave Blank if there is none): ")
 
-devicename = input("Please enter the client/devicename (leave blank to use default):")
+def main():
+    while True:
+        access_point = input("Please enter the accesspoint id (SGTIN): ").replace('-', '').upper()
+        if len(access_point) != 24:
+            print( "Invalid access_point id")
+            continue
+        break
 
-home=Home()
-home.init(access_point)
-auth = homematicip.auth.Auth(home)
+    home=Home()
+    home.init(access_point)
+    auth = homematicip.auth.Auth(home)
 
-if pin != '':
-    auth.pin=pin
+    devicename = input("Please enter the client/devicename (leave blank to use default):")
 
-if devicename != '':
-    auth.connectionRequest(access_point,devicename)
-else:
-    auth.connectionRequest(access_point)
+    while True:
+        pin = input("Please enter the PIN (leave Blank if there is none): ")
 
-print("Please press the blue button on the access point")
-while not auth.isRequestAcknowledged():
+        if pin != '':
+            auth.pin=pin
+        response = None
+        if devicename != '':
+            response = auth.connectionRequest(access_point,devicename)
+        else:
+            response = auth.connectionRequest(access_point)
+
+        if response.status_code == 200: #ConnectionRequest was fine
+            break
+
+
+        errorCode = json.loads(response.text)["errorCode"]
+        if  errorCode == "INVALID_PIN":
+            print("PIN IS INVALID!")
+        else:
+            print("Error: {}\nExiting".format(errorCode))
+            return
+
     print("Please press the blue button on the access point")
-    time.sleep(2)
+    while not auth.isRequestAcknowledged():
+        print("Please press the blue button on the access point")
+        time.sleep(2)
 
-auth_token = auth.requestAuthToken()
-clientId = auth.confirmAuthToken(auth_token)
+    auth_token = auth.requestAuthToken()
+    clientId = auth.confirmAuthToken(auth_token)
 
-print(u'-----------------------------------------------------------------------------')
-print(u'Token successfully registered!')
-print(u'AUTH_TOKEN:\t{}\nACCESS_POINT:\t{}\nClient ID:\t{}'.format(auth_token,access_point,clientId))
+    print(u'-----------------------------------------------------------------------------')
+    print(u'Token successfully registered!')
+    print(u'AUTH_TOKEN:\t{}\nACCESS_POINT:\t{}\nClient ID:\t{}'.format(auth_token,access_point,clientId))
+
+
+if __name__ == "__main__":
+    main()
