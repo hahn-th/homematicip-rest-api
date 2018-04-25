f80e3ba6e41c5b35a7f28948387f7015da68189d
coreGreenberet <green@corona-bytes.net>
2018-04-25 18:22:53 +0200

    added tests for SmokeDetector

--- tests/test_home.py
+++ tests/test_home.py
@@ -82,6 +82,7 @@ def test_shutter_device(fake_home):
     assert d.rssiDeviceValue == -64
     assert d.rssiPeerValue == None
     assert d.dutyCycle == False
+    assert d.configPending == False
     assert str(d) == 'HMIP-SWDO Fenster lowbat(False) unreach(False) rssiDeviceValue(-64) rssiPeerValue(None) configPending(False) dutyCycle(False): sabotage(False) windowState(CLOSED)'
 
 
@@ -114,8 +115,33 @@ def test_pluggable_switch_measuring(fake_home):
     assert d.availableFirmwareVersion == "0.0.0"
     assert d.firmwareVersion == "2.6.2"
     assert d.dutyCycle == False
+    assert d.configPending == False
 
     assert str(d) == 'HMIP-PSM Brunnen lowbat(None) unreach(False) rssiDeviceValue(-60) rssiPeerValue(-66) configPending(False) dutyCycle(False): on(False) profileMode(AUTOMATIC) userDesiredProfileMode(AUTOMATIC) energyCounter(0.4754) currentPowerConsumption(0.0W)'
+
+def test_smoke_detector(fake_home):
+    d = fake_home.search_device_by_id('3014F7110000000000000020')
+    assert isinstance(d, SmokeDetector)
+    assert d.label == "Rauchwarnmelder"
+    assert d.lastStatusUpdate == datetime(2018, 4, 23, 4, 5, 24, 824000) + timedelta(0,utc_offset)
+    assert d.manufacturerCode == 1
+    assert d.modelId == 296
+    assert d.modelType == "HmIP-SWSD"
+    assert d.oem == "eQ-3"
+    assert d.serializedGlobalTradeItemNumber == "3014F7110000000000000020"
+    assert d.updateState == "UP_TO_DATE"
+    assert d.lowBat == False
+    assert d.routerModuleEnabled == False
+    assert d.routerModuleSupported == False
+    assert d.rssiDeviceValue == -54
+    assert d.rssiPeerValue == None
+    assert d.unreach == False
+    assert d.smokeDetectorAlarmType == "IDLE_OFF"
+    assert d.availableFirmwareVersion == "0.0.0"
+    assert d.firmwareVersion == "1.0.11"
+    assert d.configPending == False
+    assert str(d) == 'HmIP-SWSD Rauchwarnmelder lowbat(False) unreach(False) rssiDeviceValue(-54) rssiPeerValue(None) configPending(False) dutyCycle(False): smokeDetectorAlarmType(IDLE_OFF)'
+
 
 #def test__parse_device(fake_home):
 #    assert False
