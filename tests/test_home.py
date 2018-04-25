6321d5a82553aded9bad44378daa4c3c32ba9530
coreGreenberet <green@corona-bytes.net>
2018-04-25 17:18:17 +0200

    Test cases: moved fake_home data to json file added test cases to check for ShutterContact and PluggableSwitchMeasuring

--- tests/test_home.py
+++ tests/test_home.py
@@ -4,12 +4,13 @@ import pytest
 
 from homematicip.home import Home
 from homematicip.base.base_connection import BaseConnection
-
-import json_data.home
+from homematicip.device import *
 import json
+import datetime
+
 
 def fake_home_download_configuration():
-    return json.loads(json_data.home.home_json)
+    return json.load(open("tests/json_data/home.json"))
 
 @pytest.fixture
 def fake_home():
@@ -59,6 +60,56 @@ def test_clients(fake_home):
     assert client.homeId == '00000000-0000-0000-0000-000000000001'
     assert client.id == '00000000-0000-0000-0000-000000000000'
     
+def test_shutter_device(fake_home):
+    d = fake_home.search_device_by_id('3014F7110000000000000001')
+    assert isinstance(d, ShutterContact)
+    assert d.label == "Fenster"
+    assert d.lastStatusUpdate == datetime.datetime(2018, 4, 23, 22, 37, 34, 304000)
+    assert d.manufacturerCode == 1
+    assert d.modelId == 258
+    assert d.modelType == "HMIP-SWDO"
+    assert d.oem == "eQ-3"
+    assert d.windowState == "CLOSED"
+    assert d.serializedGlobalTradeItemNumber == "3014F7110000000000000001"
+    assert d.availableFirmwareVersion == "1.16.8"
+    assert d.firmwareVersion == "1.16.8"
+    assert d.lowBat == False
+    assert d.routerModuleEnabled == False
+    assert d.routerModuleSupported == False
+    assert d.rssiDeviceValue == -64
+    assert d.rssiPeerValue == None
+    assert d.dutyCycle == False
+
+    d = fake_home.search_device_by_id('3014F7110000000000000005')
+
+    assert d.windowState == "OPEN"
+
+def test_pluggable_switch_measuring(fake_home):
+    d = fake_home.search_device_by_id('3014F7110000000000000009')
+    assert isinstance(d, PlugableSwitchMeasuring)
+    assert d.label == "Brunnen"
+    assert d.lastStatusUpdate == datetime.datetime(2018, 4, 23, 22, 36, 26, 303000)
+    assert d.manufacturerCode == 1
+    assert d.modelId == 262
+    assert d.modelType == "HMIP-PSM"
+    assert d.oem == "eQ-3"
+    assert d.serializedGlobalTradeItemNumber == "3014F7110000000000000009"
+    assert d.updateState == "UP_TO_DATE"
+    assert d.on == false
+    assert d.profileMode == "AUTOMATIC"
+    assert d.userDesiredProfileMode == "AUTOMATIC"
+    assert d.currentPowerConsumption == 0.0
+    assert d.energyCounter == 0.4754
+    assert d.lowBat == null
+    assert d.routerModuleEnabled == true
+    assert d.routerModuleSupported == true
+    assert d.rssiDeviceValue == -60
+    assert d.rssiPeerValue == -66
+    assert d.unreach == false
+    assert d.availableFirmwareVersion == "0.0.0"
+    assert d.firmwareVersion == "2.6.2"
+    assert d.dutyCycle == False
+
 #def test__parse_device(fake_home):
 #    assert False
 #
