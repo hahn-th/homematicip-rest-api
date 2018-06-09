84fd7be4a36c4f10ef4c39364957ee3ede4e7203
coreGreenberet <green@corona-bytes.net>
2018-06-09 16:29:46 +0200

    successful tests of the first basic async home functions

--- tests/async_tests/test_async_home.py
+++ tests/async_tests/test_async_home.py
@@ -22,3 +22,36 @@ async def test_async_home_base(fake_async_home: AsyncHome):
         assert fake_async_home.powerMeterUnitPrice == 0.0
         assert fake_async_home.timeZoneId == "Europe/Vienna"
         assert fake_async_home.updateState == "UP_TO_DATE"
+
+@pytest.mark.asyncio
+async def test_home_location(fake_async_home: AsyncHome):
+    assert fake_async_home.location.city == "1010  Wien, österreich"
+    assert fake_async_home.location.latitude == "48.208088"
+    assert fake_async_home.location.longitude == "16.358608"
+    assert fake_async_home.location._rawJSONData == fake_home_download_configuration()["home"]["location"]
+    assert str(fake_async_home.location) == "city(1010  Wien, österreich) latitude(48.208088) longitude(16.358608)"
+
+@pytest.mark.asyncio
+async def test_home_set_location(fake_async_home: AsyncHome):
+    with no_ssl_verification():
+        await fake_async_home.set_location("Berlin, Germany", "52.530644", "13.383068")
+        await fake_async_home.get_current_state()
+        assert fake_async_home.location.city == "Berlin, Germany"
+        assert fake_async_home.location.latitude == "52.530644"
+        assert fake_async_home.location.longitude == "13.383068"
+        assert str(fake_async_home.location) == "city(Berlin, Germany) latitude(52.530644) longitude(13.383068)"
+
+
+@pytest.mark.asyncio
+async def test_security_zones_activation(fake_async_home: AsyncHome):
+    with no_ssl_verification():
+        internal, external = fake_async_home.get_security_zones_activation()
+        assert internal == False
+        assert external == False
+
+        await fake_async_home.set_security_zones_activation(True,True)
+        await fake_async_home.get_current_state()
+
+        internal, external = fake_async_home.get_security_zones_activation()
+        assert internal == True
+        assert external == True
