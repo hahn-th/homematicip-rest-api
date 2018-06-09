7593c0cfd572b15e1135e5baddae89afe778ed6e
coreGreenberet <green@corona-bytes.net>
2018-06-09 09:05:34 +0200

    starting with the tests for the async implementation

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 if __name__ == '__main__':
     import pytest
-    pytest.main('tests --ignore=tests/async_tests/')
+    pytest.main('tests')
--- tests/conftest.py
+++ tests/conftest.py
@@ -5,9 +5,11 @@ from functools import partialmethod
 import warnings
 import requests
 import contextlib
+import aiohttp
 
 from unittest.mock import MagicMock
 from homematicip.async.connection import AsyncConnection
+from homematicip.async.home import AsyncHome
 from homematicip.home import Home
 from homematicip.connection import Connection
 
@@ -31,12 +33,23 @@ def fake_home_download_configuration():
 def no_ssl_verification():
     old_request = requests.Session.request
     requests.Session.request = partialmethod(old_request, verify=False)
+    
+    old_aiohttp_post = aiohttp.ClientSession.post
+    old_aiohttp_get = aiohttp.ClientSession.get
+    old_aiohttp_put = aiohttp.ClientSession.put
+    aiohttp.ClientSession.post = partialmethod(old_aiohttp_post,  ssl=False)
+    aiohttp.ClientSession.get = partialmethod(old_aiohttp_get,  ssl=False)
+    aiohttp.ClientSession.put = partialmethod(old_aiohttp_put,  ssl=False)
+
 
     warnings.filterwarnings('ignore', 'Unverified HTTPS request')
     yield
     warnings.resetwarnings()
 
     requests.Session.request = old_request
+    aiohttp.ClientSession.post = old_aiohttp_post
+    aiohttp.ClientSession.get = old_aiohttp_get
+    aiohttp.ClientSession.put = old_aiohttp_put
 
 
 
@@ -73,3 +86,15 @@ def async_connection(event_loop):
     _connection = AsyncConnection(event_loop)
     yield _connection
     _connection._websession.close()
+
+@pytest.fixture
+def fake_async_home(fake_cloud):
+    home = AsyncHome()
+    with no_ssl_verification():
+        lookup_url = "{}/getHost".format(fake_cloud.url)
+    #    home.download_configuration = fake_home_download_configuration
+        home._connection = AsyncConnection(0)
+        home.set_auth_token("8A45BAA53BE37E3FCA58E9976EFA4C497DAFE55DB997DB9FD685236E5E63ED7DE")
+        home._connection.init(accesspoint_id="3014F711A000000BAD0C0DED", lookup_url=lookup_url)
+        home.get_current_state()
+    return home
