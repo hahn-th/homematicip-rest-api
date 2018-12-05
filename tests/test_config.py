ce461759fe51019411010ea3958d8e92d471fa65
coreGreenberet <green@corona-bytes.net>
2018-12-05 16:11:54 +0100

    added tests for the homematic.__init__.<config functions>

---
+++ tests/test_config.py
@@ -0,0 +1,44 @@
+import pytest
+
+import homematicip
+import io,os
+import platform
+
+
+def fake_windows():
+    return "Windows"
+
+def fake_linux():
+    return "Linux"
+
+def fake_mac():
+    return "Darwin"
+
+def test_find_and_load_config_file():
+    with io.open("./config.ini", mode="w") as f:
+        f.write("[AUTH]\nauthtoken = TEMP_TOKEN\naccesspoint = TEMP_AP")
+    config = homematicip.find_and_load_config_file()
+    assert config.auth_token == "TEMP_TOKEN"
+    assert config.access_point == "TEMP_AP"
+    os.remove("./config.ini")
+
+
+def test_get_config_file_locations_win():
+    platform.system = fake_windows
+    locations = homematicip.get_config_file_locations()
+    assert len(locations) == 3 #nothing more checkable here
+   
+def test_get_config_file_locations_linux():
+    platform.system = fake_linux
+    locations = homematicip.get_config_file_locations()
+    assert locations[0] == "./config.ini"
+    assert locations[1] == "~/.homematicip-rest-api/config.ini"
+    assert locations[2] == "/etc/homematicip-rest-api/config.ini"
+   
+def test_get_config_file_locations_mac():
+    platform.system = fake_mac
+    locations = homematicip.get_config_file_locations()
+    assert locations[0] == "./config.ini"
+    assert locations[1] == "~/Library/Preferences/homematicip-rest-api/config.ini"
+    assert locations[2] == "/Library/Application Support/homematicip-rest-api/config.ini"
+    
