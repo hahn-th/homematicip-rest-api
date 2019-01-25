40c48e7d1aebefdfe9f291825227a064348f6917
coreGreenberet <green@corona-bytes.net>
2019-01-25 15:14:49 +0100

    few more comments and created mapping for intersphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -200,4 +200,4 @@ epub_exclude_files = ["search.html"]
 # -- Options for intersphinx extension ---------------------------------------
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {"https://docs.python.org/": None}
+intersphinx_mapping = { "python": ("https://python.readthedocs.io/en/latest/", None)}
