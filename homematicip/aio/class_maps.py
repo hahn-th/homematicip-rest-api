68ebffa05d166fc10bb46c4f3b544983c406e0a1
coreGreenberet <green@corona-bytes.net>
2019-05-23 18:50:58 +0200

    fixed async typo
    closes #202

--- homematicip/aio/class_maps.py
+++ homematicip/aio/class_maps.py
@@ -7,7 +7,7 @@ from homematicip.aio.securityEvent import *
 TYPE_CLASS_MAP = {
     DeviceType.DEVICE: AsyncDevice,
     DeviceType.ALARM_SIREN_INDOOR: AsyncAlarmSirenIndoor,
-    DeviceType.BRAND_BLIND:AsycBrandBlind,
+    DeviceType.BRAND_BLIND:AsyncBrandBlind,
     DeviceType.BRAND_DIMMER: AsyncBrandDimmer,
     DeviceType.BRAND_SHUTTER: AsyncFullFlushShutter,
     DeviceType.BRAND_PUSH_BUTTON: AsyncBrandPushButton,
