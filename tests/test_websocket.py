c24350b8f45ec8a0469033151cc18ec2c96dc0ce
username081581 <post@thomas-hahn.org>
2023-08-26 19:22:57 +0200

    Simplify websocket tests

--- tests/test_websocket.py
+++ tests/test_websocket.py
@@ -31,20 +31,23 @@ def home_data():
 
 
 def send_event(fake_home: Home, pushEventType: EventType, type: str, data):
+    event_data = build_json_payload(pushEventType, type, data)
+    with no_ssl_verification():
+        fake_home._restCall("ws/send", json.dumps(event_data))
+
+
+def build_json_payload(pushEventType: EventType, type: str, data) -> dict:
     if type:
         event_data = {
             "events": {"0": {"pushEventType": str(pushEventType), type: data}}
         }
     else:
         event_data = {"events": {"0": {"pushEventType": str(pushEventType)}}}
-    with no_ssl_verification():
-        fake_home._restCall("ws/send", json.dumps(event_data))
+
+    return event_data
 
 
-@pytest.mark.flaky(reruns=10, reruns_delay=4)
 def test_websocket_device(fake_home: Home, home_data):
-    fake_home.enable_events()
-
     # preparing event data for device added
     device_base_id = "3014F7110000000000000031"
     device_added = home_data["devices"][device_base_id].copy()
@@ -52,23 +55,34 @@ def test_websocket_device(fake_home: Home, home_data):
     device_added["id"] = device_added_id
     for x in device_added["functionalChannels"].values():
         x["deviceId"] = device_added_id
-    send_event(fake_home, EventType.DEVICE_ADDED, "device", device_added)
+
+    event_data = build_json_payload(EventType.DEVICE_ADDED, "device", device_added)
+    fake_home._ws_on_message(None, message=json.dumps(event_data))
 
     device_added = home_data["devices"][device_base_id].copy()
     device_added_by_change_id = "NEW_DEVICE_2"
     device_added["id"] = device_added_by_change_id
     for x in device_added["functionalChannels"].values():
         x["deviceId"] = device_added_by_change_id
-    send_event(fake_home, EventType.DEVICE_CHANGED, "device", device_added)
+
+    event_data = build_json_payload(EventType.DEVICE_CHANGED, "device", device_added)
+    fake_home._ws_on_message(None, message=json.dumps(event_data))
 
     # preparing event data for device changed
     device_changed = home_data["devices"][device_base_id].copy()
     device_changed["label"] = "CHANGED"
-    send_event(fake_home, EventType.DEVICE_CHANGED, "device", device_changed)
+
+    event_data = build_json_payload(EventType.DEVICE_CHANGED, "device", device_changed)
+    fake_home._ws_on_message(None, message=json.dumps(event_data))
+
+    # send_event(fake_home, EventType.DEVICE_CHANGED, "device", device_changed)
     # preparing event data for device remove
     device_delete_id = "3014F7110000000000BCBB11"
     assert fake_home.search_device_by_id(device_delete_id) != None
-    send_event(fake_home, EventType.DEVICE_REMOVED, "id", device_delete_id)
+
+    event_data = build_json_payload(EventType.DEVICE_REMOVED, "id", device_delete_id)
+    fake_home._ws_on_message(None, message=json.dumps(event_data))
+    # send_event(fake_home, EventType.DEVICE_REMOVED, "id", device_delete_id)
 
     time.sleep(1)
     d = fake_home.search_device_by_id(device_added_id)
@@ -85,34 +99,33 @@ def test_websocket_device(fake_home: Home, home_data):
 
     assert fake_home.search_device_by_id(device_delete_id) is None
 
-    fake_home.disable_events()
 
-
-@pytest.mark.flaky(reruns=10, reruns_delay=4)
 def test_websocket_group(fake_home: Home, home_data):
-    fake_home.enable_events()
-
     # preparing event data for group added
     group_base_id = "00000000-0000-0000-0000-000000000020"
     group_added = home_data["groups"][group_base_id].copy()
     group_added_id = "NEW_group"
     group_added["id"] = group_added_id
-    send_event(fake_home, EventType.GROUP_ADDED, "group", group_added)
+    payload = build_json_payload(EventType.GROUP_ADDED, "group", group_added)
+    fake_home._ws_on_message(None, json.dumps(payload))
 
     group_added = home_data["groups"][group_base_id].copy()
     group_added_by_change_id = "NEW_group_2"
     group_added["id"] = group_added_by_change_id
-    send_event(fake_home, EventType.GROUP_CHANGED, "group", group_added)
+    payload = build_json_payload(EventType.GROUP_CHANGED, "group", group_added)
+    fake_home._ws_on_message(None, json.dumps(payload))
 
     # preparing event data for group changed
     group_changed_id = "00000000-0000-0000-0000-000000000016"
     group_changed = home_data["groups"][group_changed_id].copy()
     group_changed["label"] = "CHANGED"
-    send_event(fake_home, EventType.GROUP_CHANGED, "group", group_changed)
+    payload = build_json_payload(EventType.GROUP_CHANGED, "group", group_changed)
+    fake_home._ws_on_message(None, json.dumps(payload))
     # preparing event data for group remove
     group_delete_id = "00000000-0000-0000-0000-000000000012"
     assert fake_home.search_group_by_id(group_delete_id) != None
-    send_event(fake_home, EventType.GROUP_REMOVED, "id", group_delete_id)
+    payload = build_json_payload(EventType.GROUP_REMOVED, "id", group_delete_id)
+    fake_home._ws_on_message(None, json.dumps(payload))
 
     time.sleep(1)
     d = fake_home.search_group_by_id(group_added_id)
@@ -129,52 +142,47 @@ def test_websocket_group(fake_home: Home, home_data):
 
     assert fake_home.search_group_by_id(group_delete_id) is None
 
-    fake_home.disable_events()
 
-
-@pytest.mark.flaky(reruns=10, reruns_delay=4)
 def test_websocket_security_journal_changed(fake_home: Home, home_data):
-    fake_home.enable_events()
-    send_event(fake_home, EventType.SECURITY_JOURNAL_CHANGED, None, None)
+    payload = build_json_payload(EventType.SECURITY_JOURNAL_CHANGED, None, None)
+    fake_home._ws_on_message(None, json.dumps(payload))
     time.sleep(1)
-    fake_home.disable_events()
 
 
-@pytest.mark.flaky(reruns=10, reruns_delay=4)
 def test_websocket_home_changed(fake_home: Home, home_data):
-    fake_home.enable_events()
     new_home = home_data["home"].copy()
     new_home["weather"]["humidity"] = 60
 
     assert fake_home.weather.humidity == 54
 
-    send_event(fake_home, EventType.HOME_CHANGED, "home", new_home)
+    payload = build_json_payload(EventType.HOME_CHANGED, "home", new_home)
+    fake_home._ws_on_message(None, json.dumps(payload))
+
     time.sleep(1)
     assert fake_home.weather.humidity == 60
-    fake_home.disable_events()
 
 
-@pytest.mark.flaky(reruns=10, reruns_delay=4)
 def test_websocket_client(fake_home: Home, home_data):
-    fake_home.enable_events()
-
     # preparing event data for client added
     client_base_id = "00000000-0000-0000-0000-000000000000"
     client_added = home_data["clients"][client_base_id].copy()
     client_added_id = "NEW_CLIENT"
     client_added["id"] = client_added_id
-    send_event(fake_home, EventType.CLIENT_ADDED, "client", client_added)
+    payload = build_json_payload(EventType.CLIENT_ADDED, "client", client_added)
+    fake_home._ws_on_message(None, json.dumps(payload))
 
     # preparing event data for client changed
     client_changed = home_data["clients"][client_base_id].copy()
     client_changed["label"] = "CHANGED"
-    send_event(fake_home, EventType.CLIENT_CHANGED, "client", client_changed)
+    payload = build_json_payload(EventType.CLIENT_CHANGED, "client", client_changed)
+    fake_home._ws_on_message(None, json.dumps(payload))
     # preparing event data for client remove
     client_delete_id = "AA000000-0000-0000-0000-000000000000"
     c = fake_home.search_client_by_id(client_delete_id)
     assert c != None
     assert c.label == "REMOVE_ME"
-    send_event(fake_home, EventType.CLIENT_REMOVED, "id", client_delete_id)
+    payload = build_json_payload(EventType.CLIENT_REMOVED, "id", client_delete_id)
+    fake_home._ws_on_message(None, json.dumps(payload))
 
     time.sleep(1)
     d = fake_home.search_client_by_id(client_added_id)
@@ -186,8 +194,6 @@ def test_websocket_client(fake_home: Home, home_data):
     assert isinstance(d, Client)
 
     assert fake_home.search_client_by_id(client_delete_id) is None
-
-    fake_home.disable_events()
 
 
 ws_error_called = False
