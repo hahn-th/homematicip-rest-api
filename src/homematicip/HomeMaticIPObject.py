94819f98ab0e18d9e8404b0988c1a02cab05ee0a
Thomas Hahn <post@thomas-hahn.org>
2025-07-18 20:38:16 +0200

    remove deprecated file

--- src/homematicip/HomeMaticIPObject.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-LOGGER = logging.getLogger(__name__)
-
-LOGGER.warning(
-    DeprecationWarning(
-        "homematicip.HomeMaticIPObject is deprecated in favor of homematicip.base.HomeMaticIPObject"
-    )
-)
